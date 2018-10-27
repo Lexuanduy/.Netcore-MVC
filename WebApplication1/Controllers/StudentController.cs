1840c8535c8d2fd6af272d9a52d383b5e1d5987d
Lexuanduy <lexuanduy07@gmail.com>
2018-10-27 21:26:55 +0700

    create first project

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext _context;
+
+        public StudentController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return new JsonResult(_context.students.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Store(Student student)
+        {
+            _context.students.Add(student);
+            _context.SaveChanges();
+            return new JsonResult(student);
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String RollNumber { get; set; }
+    }
+}
